a776946c49233a9527395b168747a4b23356ea45
marcof90 <ingmarcosanabria@gmail.com>
2021-10-21 18:08:27 -0500

    Heroku first push

--- src/app.js
+++ src/app.js
@@ -2,11 +2,15 @@ const express = require('express')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
 const cors = require('cors')
+require('dotenv').config()
 const app = express()
 const authRoutes = require('./routes/auth.routes')
 
 //configuraciones
 app.set('port', process.env.PORT || 3000)
+mongoose.connect(process.env.DB_STRING)
+.then(db => console.log('Connected to Mongo'))
+.catch(err => console.log(err))
 
 //middlewares
 app.use(morgan('dev'))
